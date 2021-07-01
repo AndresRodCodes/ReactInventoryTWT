bdc3ccff9b8ffffc6fae899629bbb74f5e30b909
unknown <andres6459@gmail.com>
2021-06-30 17:32:06 -0700

    Added json-server database. Added post and delete requests.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import SearchBar from "./SearchBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddItem from "./AddItem";
 import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
   const [filters, setfilters] = useState({});
   const [items, setItems] = useState({ items: [] });
+  const jsonServerDB = "http://localhost:3000/items";
+
+  useEffect(() => {
+    fetch(jsonServerDB)
+      .then((response) => response.json())
+      .then((data) => 
+        setItems({ items: data }));
+  }, []);
 
   const updateFilters = (searchParams) => {
     setfilters(searchParams);
@@ -14,17 +22,46 @@ function App() {
 
   const addItemData = (item) => {
     let currentData = items["items"];
-    item.id = currentData.length;
-    currentData.push(item);
-    setItems({ items: currentData });
-    console.log(items);
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+
+    fetch(jsonServerDB, fetchOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        currentData.push(data);
+        setItems({ items: currentData });
+        console.log(currentData);
+      });
+  };
+
+  const deleteItem = (item) => {
+    const itemsList = items["items"];
+    const requestOptions = {
+      method : "DELETE"
+    };
+    fetch(jsonServerDB + `/${item.id}`, requestOptions).then((response) => {
+      if (response.ok) {
+        const index = itemsList.indexOf(item);
+        itemsList.splice(index, 1);
+        setItems({ items: itemsList });
+      }
+    })
   };
 
   const filterItems = (data) => {
     const filteredItems = [];
 
-    for (const item of data) {
+    if (!filters.name) {
+      return data;
+    }
 
+    for (const item of data) {
       if (filters.name !== "" && item.name !== filters.name) {
         continue;
       }
@@ -53,7 +90,7 @@ function App() {
         <AddItem addItem={addItemData} />
       </div>
       <div className="row">
-        <ItemsDisplay items={filterItems(items["items"])} />
+        <ItemsDisplay items={filterItems(items["items"])} deleteItem={deleteItem} />
       </div>
     </div>
   );
