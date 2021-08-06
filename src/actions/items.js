78b878c20addcab5b8a48b42100a52bfcd1d6b79
unknown <andres6459@gmail.com>
2021-08-06 15:24:07 -0700

    Added action item constraints to client actions/items.js

--- src/actions/items.js
+++ src/actions/items.js
@@ -1,3 +1,9 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  DELETE_ITEM,
+  UPDATE_ITEM,
+} from "../constants/actionTypes";
 import * as api from "../api";
 
 // Item Action Creators
@@ -5,7 +11,7 @@ export const getItems = () => async (dispatch) => {
   try {
     const { data } = await api.fetchItems();
 
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -15,7 +21,7 @@ export const createItem = (item) => async (dispatch) => {
   try {
     const { data } = await api.createItem(item);
 
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -25,7 +31,7 @@ export const deleteItem = (id) => async (dispatch) => {
   try {
     await api.deleteItem(id);
 
-    dispatch({ type: "DELETE_ITEM", payload: id });
+    dispatch({ type: DELETE_ITEM, payload: id });
   } catch (error) {
     console.log(error.message);
   }
@@ -35,7 +41,7 @@ export const updateItem = (id, item) => async (dispatch) => {
   try {
     const { data } = await api.updateItem(id, item);
 
-    dispatch({ type: "UPDATE_ITEM", payload: data });
+    dispatch({ type: UPDATE_ITEM, payload: data });
   } catch (error) {
     console.log(error);
   }
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,4 @@
+export const FETCH_ALL = "FETCH_ALL";
+export const CREATE = "CREATE";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
