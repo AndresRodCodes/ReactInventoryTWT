3558d485491bf839872a06525be17eac1d6fccad
unknown <andres6459@gmail.com>
2021-08-06 15:26:50 -0700

    Added action constants to the reducers.

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,14 +1,21 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  DELETE_ITEM,
+  UPDATE_ITEM,
+} from "../constants/actionTypes";
+
 export default (items = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...items, action.payload];
-    case "UPDATE_ITEM":
+    case UPDATE_ITEM:
       return items.map((item) =>
         item._id === action.payload._id ? action.payload : item
       );
-    case "DELETE_ITEM":
+    case DELETE_ITEM:
       return items.filter((item) => item._id !== action.payload);
     default:
       return items;
